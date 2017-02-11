68274e0313bd5c33797a1838cee2d0c372d5c474
Ryan Yoon <Ryan Yoon>
2017-02-11 05:01:58 -0500

    backbone of main page

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name='description' content='Fire Emblem Heroes Catalog'>
+    <meta name='keywords' content='Fire, Emblem, Heroes, Catalog'>
+    <meta name='author' content='Ryan Yoon'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>FE Heroes</title>
+    <link rel='stylesheet' type='text/css' href='styles/styles.css'>
+</head>    
+
+<body>
+    <div id='title_container' class='container'>
+        <!-- http://fireemblemheroeshack.com/img/logo.png -->
+        <img src="images/title.jpg">
+    </div>
+
+    <div id='add_search_container' class='container'>
+        <div id='add_container'>
+            
+        </div>
+
+        <div id='search_container'>
+            
+        </div>
+    </div>
+
+    <div id='button_container' class='container'>
+        <button type='button'>View the Heroes Below!</button>        
+    </div>
+    
+
+    <!-- <a id='top_arrow' href='#title'>UP</a> -->
+</body>
+</html>
